819175f93d37be570bd083551c957769835e207b
dtkiet1503 <dangtuankiet1503@gmail.com>
2023-05-03 10:03:11 +0700

    New Project

---
+++ StudentManagement-frontend/src/components/AddClassForm.vue
@@ -0,0 +1,79 @@
+<script>
+
+export default {
+    props: {
+        classes: { type: Object , required: true},
+    },
+    
+    emits: ["submit:class"],
+    data(){
+        return {classLocal: this.classes}
+    },
+    methods: {
+        submit() {
+            this.$emit("submit:class", this.classLocal);
+        },
+    },
+};
+</script>
+
+<template>
+    <h4 class="text-center">Thêm ca làm</h4>
+    <div class="w-50 mx-auto py-4 px-5 bg-light login-form">
+        <div class="mb-3">
+            <label for="" class="form-label">Ca làm việc</label>
+            <select v-model="classLocal.subject" class="custom-select" aria-label="Default select example">
+                <option value="Ca Sáng">Ca Sáng</option>
+                <option value="Ca Trưa">Ca Trưa</option>
+                <option value="Ca Sáng">Ca Tối</option>
+            </select>
+        </div>
+        <div class="mb-3">
+            <label for="" class="form-label">Ký hiệu</label>
+            <input v-model="classLocal.name" type="text" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label for="" class="form-label">Thứ</label>
+            <select v-model="classLocal.day" class="custom-select" aria-label="Default select example">
+                <option value="Thứ hai">Thứ hai</option>
+                <option value="Thứ ba">Thứ ba</option>
+                <option value="Thứ tư">Thứ tư</option>
+                <option value="Thứ năm">Thứ năm</option>
+                <option value="Thứ sáu">Thứ sáu</option>
+                <option value="Thứ bảy">Thứ bảy</option>
+                <option value="Chủ nhật">Chủ nhật</option>
+            </select>
+        </div>
+        <div class="mb-3">
+            <label for="" class="form-label">Phòng</label>
+            <select v-model="classLocal.room" class="custom-select" aria-label="Default select example">
+                <option value="Phòng 101">Phòng 101</option>
+                <option value="Phòng 102">Phòng 102</option>
+                <option value="Phòng 302">Phòng 302</option>
+                <option value="Phòng 201">Phòng 201</option>
+                <option value="Phòng 205">Phòng 205</option>
+                <option value="Phòng 202">Phòng 202</option>
+            </select>
+        </div>
+        <div class="mb-3">
+            <label for="" class="form-label">Ca</label>
+            <select v-model="classLocal.time" class="custom-select" aria-label="Default select example">
+                <option value="Ca 1(7h-9h)">Ca 1(7h-9h)</option>
+                <option value="Ca 2(9h-11h)">Ca 2(9h-11h)</option>
+                <option value="Ca 3(11h-1h)">Ca 3(11h-1h)</option>
+                <option value="Ca 4(1-3h)">Ca 4(1h-3h)</option>
+                <option value="Ca 5(3h-5h)">Ca 5(5h-7h)</option>
+                <option value="Ca 6(7h-9h)">Ca 6(7h-9h)</option>
+            </select>
+        </div>
+        <div class="mb-3">
+            <label for="" class="form-label">Số giờ làm</label>
+            <input v-model="classLocal.long" type="number" class="form-control">
+        </div>
+        
+        <div class="d-flex justify-content-end">
+            <a><button @click="submit" type="submit" class="btn btn-primary">Thêm</button></a>
+        </div>
+    </div>
+</template>
+
---
+++ StudentManagement-frontend/src/components/AddProfessorForm.vue
@@ -0,0 +1,68 @@
+<script>
+
+export default {
+    props: {
+        professor: { type: Object , required: true},
+    },
+    
+    emits: ["submit:professor"],
+    data(){
+        return {professorLocal: this.professor}
+    },
+    methods: {
+        submit() {
+            this.$emit("submit:professor", this.professorLocal);
+        },
+    },
+};
+</script>
+
+<template>
+    <h4 class="text-center">Thêm giảng viên</h4>
+    <div class="w-50 mx-auto py-4 px-5 bg-light login-form">
+        <div class="mb-3">
+            <label for="" class="form-label">Họ tên</label>
+            <input v-model="professorLocal.name" type="text" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label for="" class="form-label">Tên tài khoản</label>
+            <input v-model="professorLocal.account.username" type="text" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label for="" class="form-label">Mật khẩu</label>
+            <input v-model="professorLocal.account.password" type="password" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label for="" class="form-label">Ngày sinh</label>
+            <input v-model="professorLocal.birth" type="date" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label for="" class="form-label">Quê quán</label>
+            <input v-model="professorLocal.hometown" type="text" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label for="" class="form-label">Số năm làm việc</label>
+            <select v-model="professorLocal.unit" class="custom-select" aria-label="Default select example">
+                <option value="2 năm">2 năm</option>
+                <option value="3 năm">3 năm</option>
+                <option value="4 năm">4 năm</option>
+                <option value="5 năm">5 năm</option>
+                <option value="hơn 5 năm">hơn 5 năm</option>
+            </select>
+        </div>
+        <div class="mb-3">
+            <label for="" class="form-label">Bộ phận</label>
+            <select v-model="professorLocal.college" class="custom-select" aria-label="Default select example">
+                <option value="Pha Chế">Pha Chế</option>
+                <option value="Tạp Vụ">Tạp Vụ</option>
+                <option value="Bán Hàng">Bán Hàng</option>
+                <option value="Phục Vụ">Phục Vụ</option>
+                <option value="Bếp">Bếp</option>
+            </select>
+        </div>
+        <div class="d-flex justify-content-end">
+            <a><button @click="submit" type="submit" class="btn btn-primary">Thêm</button></a>
+        </div>
+    </div>
+</template>
+
